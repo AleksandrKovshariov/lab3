a5aac7cbd2a1fb299d2d534353e4cee8ae0dc1e1
kovsharov <sanforg2@gmail.com>
2021-04-17 14:23:56 +0300

    Add registration flow

--- FlaskServer.py
+++ FlaskServer.py
@@ -2,9 +2,16 @@ from flask_script import Server
 from log import setup_logger
 import logging
 from decouple import config
+import requests
 
 setup_logger('root')
 logger = logging.getLogger('root')
+
+
+class Config:
+    apiKey = config('API_KEY')
+    externalServerUrl = config('EVENT_SERVICE_URL')
+    roomNumber = config('ROOM_NUMBER')
 
 
 class FlaskServer(Server):
@@ -12,6 +19,6 @@ class FlaskServer(Server):
         self.init()
         return Server.__call__(self, app, *args, **kwargs)
 
-    def init(self):
-        API_KEY = config('API_KEY')
-        print(API_KEY)
+    @staticmethod
+    def init():
+        requests.post(f'{Config.externalServerUrl}/numbers/{Config.roomNumber}/register', headers={'API-Key': Config.apiKey})
