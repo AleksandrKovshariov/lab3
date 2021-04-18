1429811a6ceb888f84b96267e3341e9df5ae1c17
kovsharov <sanforg2@gmail.com>
2021-04-18 17:34:50 +0300

    Add idea to gitignore

--- services/EventService.py
+++ services/EventService.py
@@ -2,8 +2,9 @@ import requests
 import logging
 
 from Config import Config
-from dtos.EventDTO import EventDTO, SensorType, EventType
+from dtos.EventDTO import EventDTO, SensorType
 from dtos.sensors.DistanceSensorDTO import DistanceSensorDTO, DistanceSensorType
+from dtos.sensors.EventType import EventType
 from services.JsonConvert import JsonConvert
 
 logger = logging.getLogger('root')
@@ -13,7 +14,7 @@ class EventService:
 
     @staticmethod
     def send_distance_sensor_event(sensor_type: DistanceSensorType):
-        data = JsonConvert.to_json(EventDTO(EventType.TRIGGERED, SensorType.DISTANCE, DistanceSensorDTO(sensor_type)))
+        data = JsonConvert.to_json(EventDTO(SensorType.DISTANCE, DistanceSensorDTO(EventType.TRIGGERED, sensor_type)))
         logger.info(f"Sending event: distance sensor event {data}")
         requests.post(f'{Config.externalServerUrl}/events/distance',
                       headers={'API-Key': Config.apiKey, 'Content-type': 'application/json'},
