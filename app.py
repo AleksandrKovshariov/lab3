5d481c42bd5d0adbc7130d4633eb07bc90ad42f3
kovsharov <sanforg2@gmail.com>
2021-05-15 10:45:56 +0300

    Push

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_script import Manager
 
 from FlaskServer import FlaskServer
+from services.ComponentService import ComponentService
 
 app = Flask(__name__)
 manager = Manager(app)
 
 manager.add_command('runserver', FlaskServer())
 
-
 @app.route('/')
 def hello_world():
+    ComponentService().lock_door()
     return 'Hello World!'
 
 
--- services/ComponentService.py
+++ services/ComponentService.py
@@ -1,12 +1,29 @@
+import logging
+
 from components.sensors.DistanceSensor import DistanceSensor
+from components.RGB import RGB
 
 try:
     import RPi.GPIO
 except (RuntimeError, ModuleNotFoundError):
     import fake_rpigpio.utils
+
     fake_rpigpio.utils.install()
 
+logger = logging.getLogger('root')
 
-class ComponentService:
-    left_distance_sensor = DistanceSensor(21)
-    right_distance_sensor = DistanceSensor(25)
+
+class ComponentService(object):
+    _instance = None
+
+    def __new__(self):
+        if not self._instance:
+            self._instance = super(ComponentService, self).__new__(self)
+            self.left_distance_sensor = DistanceSensor(21)
+            self.right_distance_sensor = DistanceSensor(25)
+            self.rgb = RGB(23, 19, 26)
+        return self._instance
+
+    def lock_door(self):
+        logger.log('Locking door')
+        self.rgb.red()
