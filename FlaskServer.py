1e9a7dbb93769544624fb8c887f83d4b128c9aa1
kovsharov <sanforg2@gmail.com>
2021-11-07 11:44:14 +0200

    Test

--- FlaskServer.py
+++ FlaskServer.py
@@ -1,9 +1,5 @@
 from functools import partial
 
-from Config import Config
-from dtos.sensors.DistanceSensorDTO import DistanceSensorType
-from services.ComponentService import ComponentService
-from services.EventService import EventService
 from flask_script import Server
 from log import setup_logger
 import requests
